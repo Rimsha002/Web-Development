3a4510d9c2e5e24ebe25f38201972ec1041476a2
Rimsha002 <rimshamajeed2002@gmail.com>
2025-03-30 09:51:45 +0500

    Fixed preProject issue

---
+++ React-App/preProject/README.md
@@ -0,0 +1,3 @@
+# React + TypeScript + Vite Practice Projects
+
+It is the Practice projects for React.js Course
---
+++ React-App/preProject/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="mb-4 self-end " >Hello World</h1>
+      <p>Rimsha Majeed</p>
+    </>
+  )
+}
+
+export default App
